331db26df2c988ad12179565dd6dd606fcff5059
Rignchen <rinchendemort@gmail.com>
2025-03-24 15:08:18 +0100

    feat: display all the related data
    the iframe don't work but I can already work with it

--- src/app/pages/walk-detail/walk-detail.component.ts
+++ src/app/pages/walk-detail/walk-detail.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import Walk from '../../../types/walk'
 
 @Component({
-  selector: 'app-walk-detail',
-  imports: [],
-  templateUrl: './walk-detail.component.html',
-  styleUrl: './walk-detail.component.scss'
+	selector: 'app-walk-detail',
+	imports: [],
+	templateUrl: './walk-detail.component.html',
+	styleUrl: './walk-detail.component.scss'
 })
 export class WalkDetailComponent {
-  public walk: Walk = new Walk();
-  constructor(private route: ActivatedRoute) {}
+	public walk: Walk = new Walk();
+	constructor(private route: ActivatedRoute, public location: Location) {}
 
-  ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      let id: string = params['id'];
-      if (!/^\d+$/.test(id)) {
-        throw new Error('Invalid id');
-      }
-      this.walk.fromID(parseInt(id));
-    });
-  }
+	ngOnInit(): void {
+		this.route.params.subscribe((params) => {
+			let id: string = params['id'];
+			if (!/^\d+$/.test(id)) {
+				throw new Error('Invalid id');
+			}
+			this.walk.fromID(parseInt(id));
+		});
+	}
 }
