1556d5a9c47bf6db95f540374028aba3638d2a54
Rignchen <rinchendemort@gmail.com>
2025-03-24 10:17:06 +0100

    feat(walk): route to page

--- src/app/pages/walk-detail/walk-detail.component.ts
+++ src/app/pages/walk-detail/walk-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-walk-detail',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './walk-detail.component.scss'
 })
 export class WalkDetailComponent {
+  public id!: number;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      let id: string = params['id'];
+      if (!/^\d+$/.test(id)) {
+        throw new Error('Invalid id');
+      }
+      this.id = parseInt(id);
+    });
+  }
 }
