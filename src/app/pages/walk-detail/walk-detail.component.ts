73556c160def2c800bc2735220eedb06d00d50b4
Rignchen <rinchendemort@gmail.com>
2025-03-26 09:59:07 +0100

    feat(walk): display correctly on mobile phone
    I didn't manage to make the button green

--- src/app/pages/walk-detail/walk-detail.component.ts
+++ src/app/pages/walk-detail/walk-detail.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import Walk from '#types/walk.js';
 import { BackButtonComponent } from '#components/back-button/back-button.component.js';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
 	selector: 'app-walk-detail',
-	imports: [BackButtonComponent],
+	imports: [BackButtonComponent, MatButtonModule, MatIconModule],
 	templateUrl: './walk-detail.component.html',
 	styleUrl: './walk-detail.component.scss'
 })
