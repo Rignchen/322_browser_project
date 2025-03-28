6f7ad785f0609a017004d4fa7c53adc99a9882eb
d-aaf <fabdulazeez@vaudoise.ch>
2025-03-28 10:46:55 +0100

    modified app according PR comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Error404Component } from './components/error404/error404.component';
 import { Error500Component } from './components/error500/error500.component';
-import { HomeComponent } from './components/home/home.component';
 
 
 export const routes: Routes = [
 
-    {path: '',component: HomeComponent},
     {path: '500',component: Error500Component},
     {path: '**' ,component: Error404Component},
     
